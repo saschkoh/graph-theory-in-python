b95ba6066260e801b7f87e14dd5fb3c7bb1a2ffc
saschkoh <sascha.kohler@posteo.de>
2023-06-24 23:47:23 +0200

    add oellrich graph

--- graph_explorations/core.py
+++ graph_explorations/core.py
@@ -1,28 +1,29 @@
 """
 This module contains the core functionality for the graph_explorations package
 """
-from graph.core import Node, Edge, Graph
+from oellrich_graph_in_python import Node, Edge, Graph
 
 import heapq
 
 
 class Dijkstra:
     """
-    This class implements naive and modified Dijkstra's algorithms to find shortest paths in a graph.
+    This class implements naive and modified Dijkstra's algorithms to find shortest paths in a
+    graph.
     """
     def __init__(self, graph: Graph = None):
         self.graph = graph
 
-    def back_diff(self, target_idx: int = None):
+    def min_distances(self, target_idx: int = None):
         """
-        This method finds the shortest path betwen a target node and all other nodes in the graph using the backward
-        difference.
+        This method finds the shortest path betwen a target node and all other nodes in the graph
+        using the backward difference.
         """
         # initialize the distances vector
-        distances = [float("inf") for _ in range(len(self.graph.node_count))]
+        distances = [float("inf") for _ in range(self.graph.node_count)]
         distances[target_idx] = 0
         # initialize the backward neighbors heap
-        heap = [(dict[target_idx], target_idx)]
+        heap = [(distances[target_idx], target_idx)]
         while heap:
             _, node_idx = heapq.heappop(heap)
             for edge in self.graph.nodes[node_idx].b_edges:
@@ -33,18 +34,10 @@ class Dijkstra:
 
     def shortest_path(self, source_idx: int = None, target_idx: int = None):
         """
-        This method finds a shortest path between source and and target_idx node using Dijkstra's algorithm
+        This method finds a shortest path between source and and target node using Dijkstra's
+        algorithm.
         """
-        distances = [float("inf") for _ in range(len(self.graph.node_count))]
-        distances[source_idx] = 0
-        # initialize the forward nodes heap
-        heap = [(dict[source_idx], source_idx)]
-        while heap:
-            _, node_idx = heapq.heappop(heap)
-            for edge in self.graph.nodes[node_idx].f_edges:
-                if distances[edge.tail.index] > distances[node_idx] + edge.weight:
-                    distances[edge.tail.index] = distances[node_idx] + edge.weight
-                    heapq.heappush(heap, (distances[edge.weight], edge.tail.index))
+        distances = self.min_distances(source_idx)
         return distances[target_idx]
 
 
@@ -55,14 +48,14 @@ if __name__ == "__main__":
     edge_ab = Edge("AB", node_a, node_b, 0, 1)
     edge_bc = Edge("BC", node_b, node_c, 1, 2)
 
-    graph = Graph(
+    test_graph = Graph(
         "test_graph",
         True,
         [node_a, node_b, node_c],
         [edge_ab, edge_bc]
     )
-    dijkstra = Dijkstra(graph)
-    back_diffs = dijkstra.back_diff(2)
+    dijkstra = Dijkstra(test_graph)
+    back_diffs = dijkstra.min_distances(2)
     s_t_dist = dijkstra.shortest_path(0, 2)
     print(f"backward difference distances = {back_diffs}")
-    print(f"s-t shortest distance")
+    print(f"s-t shortest distance = {s_t_dist}")
